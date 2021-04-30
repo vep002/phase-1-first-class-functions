c8f989238782fa6a0af8b2812a665c3488c98ae5
Victoria <vep002@gmail.com>
2021-04-30 16:37:24 -0500

    commit tree c0e05d5505b9956078786cfc19ff12b679ff5b80

---
+++ index.js
@@ -0,0 +1,48 @@
+//  function Monday(){
+//     console.log('Go for a five-mile run');
+//     console.log ('Pump iron');
+// }
+
+// function Tuesday() {
+//     console.log('Go for a five-mile run');
+//     console.log('Swim 40 laps');
+// }
+// function Wednesday () {
+//     console.log('Go for a five-mile run');
+//     console.log('Go for a five-mile run');
+// }
+
+// function Thursday () {
+//     console.log('Go for a five-mile run');
+//     console.log('Pump iron');
+// }
+
+// function Friday() {
+//     console.log('Go for a five-mile run');
+//     console.log('Swim 40 laps');
+// }
+
+// function runFiveMiles() {
+//     console.log('Go for a five-mile run')
+// }
+// function liftWeights() {
+//     console.log('Pump iron');
+//   }
+// function swimFortyLaps() {
+//     console.log('Swim 40 laps');
+//   }
+// function Monday() {
+//     runFiveMiles();
+//     liftWeights();
+//   }
+
+// function exerciseRoutine(postRunActivity) {
+//     runFiveMiles();
+//     postRunActivity();
+// }
+// function Monday() {
+//     exerciseRoutine(liftWeigts);
+// }
+
+// Monday();
+
